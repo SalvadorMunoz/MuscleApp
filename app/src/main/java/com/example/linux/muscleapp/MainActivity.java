d0f9ce5b3fbb37a9e2a6310b1c664d1f2c8f00b0
SalvadorMunoz <salvi.8_94@hotmail.com>
2017-10-18 17:42:31 +0200

    Camera added

--- app/src/main/java/com/example/linux/muscleapp/MainActivity.java
+++ app/src/main/java/com/example/linux/muscleapp/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.linux.muscleapp;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -19,6 +26,12 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     //Create and inflate container
     @BindView(R.id.srlContainer) SwipeRefreshLayout swipeContainer;
     @BindView(R.id.hola) TextView hola;
+    @BindView(R.id.fbtVideo) FloatingActionButton fbtVideo;
+
+    //Camera intent id and camera limit
+    private static final int CAMERA_ID =1;
+    private static final int VIDEO_LIMIT=30;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,9 +43,20 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         //Change swipe load color
         swipeContainer.setColorSchemeColors(getResources().getColor(R.color.colorPrimary));
 
+        fbtVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Create camera intent and set limit
+                Intent cameraintent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                cameraintent.putExtra(MediaStore.EXTRA_DURATION_LIMIT,VIDEO_LIMIT);
+                //Launch camera
+                startActivityForResult(cameraintent,CAMERA_ID);
+            }
+        });
+
     }
 
-
+    //Change tag when swipe activity refresh
     @Override
     public void onRefresh() {
         new Handler().postDelayed(new Runnable() {
@@ -42,6 +66,22 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                 swipeContainer.setRefreshing(false);
             }
         },2000);
+
+    }
+
+    //See the results of callbacks intent
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //Check the activity result
+        switch (requestCode){
+            case CAMERA_ID:
+                if(resultCode == Dialog.BUTTON_POSITIVE)
+                    hola.setText("Video grabado...");
+                break;
+        }
+
 
     }
 }
